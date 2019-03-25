a61d84895a2121e9b718324df908a6fe207d309a
DanielDumile <daniel_gn98@hotmail.com>
2019-03-25 16:36:03 -0600

    Update P6.py

--- 2doParcial/Sesion6/P6.py
+++ 2doParcial/Sesion6/P6.py
@@ -1,24 +1,31 @@
-def basica(b,m):
-    tam = log2(b)+1
-    b = (b<<1);
-    if(b&(1 << tam)):
-        b = b^(m^(1<<tam))
+import math
+
+def basic(b,m):
+    ult = int(math.log2(m))
+    print("El ultimo es "+ult)
+    flag = False
+    b = b << 1
+    if (b&(1<<ult)):
+        b = b ^ (1<<ult)
+        flag = True
+    if(flag):
+        b = b^(m^(1 << (ult)))
     return b
     
-def normal(a,b,m):
-    tam = log2(a)+1
+def mul(a,b,m):
+    tam = int(math.log2(a))+1
     i = 0
     res = 0
     while (i < tam):
         if(a &(1<<i)):
             aux = b
             for j in range(i):
-                aux = basica(aux,m)
+                aux = basic(aux,m)
             res = res ^ aux
-            i= i+1
+        i= i+1
     return res
 
-def representation(s):
+def getPoly(s):
     tam = int(len(s))
     res = ""
     for i in range(tam):
@@ -32,8 +39,49 @@ def representation(s):
                 res += ("x^" + str(tam-i-1) + "+")
     return res[0:len(res)-1]
 
+def getNumber(s):
+    tam = int(len(s))
+    ans = 0
+    for i in range(tam):
+        if(s[i] == '1'):
+            pos = tam-i-1
+            ans += pow(2,pos)
+    return ans
+
+def getString(a):
+    tam = int(math.log2(a))+1
+    ans = ""
+    for i in range(tam):
+        if(a & (1<<i)):
+            ans+="1"
+        else:
+            ans+="0"
+    return ans[::-1]
+
+
 def main():
-    print(representation("1100110"))
+
+    while True:
+        a = input("Enter number A: ")
+        a = int("0x"+a,0)
+        print(a)
+        print(getString(a))
+        b = input("Enter number B: ")
+        b = int("0x"+b,0)
+        print(b)
+        print(getString(b))
+        #print(getPoly(a))
+        #a = int(getNumber(a))
+        #print(getPoly(b))
+        #b = int(getNumber(b))
+        #print(getPoly(mod))
+        mod = int(getNumber("100011011"))
+        mod = int(pow(2,8)+pow(2,4)+pow(2,3)+pow(2,1)+pow(2,0))
+        #print(basic(b,mod))
+        print("Resultado:")
+        res = mul(a,b,mod)
+        print(hex(res))
+        #print(getPoly(getString(mul(a,b,mod))))
     
 main()
     
