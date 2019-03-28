0ab862b35c8c015db4e1fa67ba73ca688e8c2455
DanielDumile <daniel_gn98@hotmail.com>
2019-03-28 10:07:53 -0600

    Update P6.py

--- 2doParcial/Sesion6/P6.py
+++ 2doParcial/Sesion6/P6.py
@@ -1,4 +1,5 @@
 import math
+import subprocess
 #mod = 100011010
 #mod = int(pow(2,8)+pow(2,4)+pow(2,3)+pow(2,1)+pow(2,0))
 
@@ -62,15 +63,19 @@ def getString(a):
             ans+="0"
     return ans[::-1]
 
-def computeMultiplicativeInverse(aux,a,mod):
-    for i in range(253):
-        a = mul(aux,a,mod)
-    return a
+def binPow(a,b,mod):
+	res = 1
+	while(b > 0):
+		if(b & 1):
+			res = mul(res,a,mod)
+		a = mul(a,a,mod)
+		b >>= 1
+	return res
 
 def getMultiplicativeInverse(a):
     a = int("0x"+a,0)
     mod = int(pow(2,8)+pow(2,4)+pow(2,3)+pow(2,1)+pow(2,0))
-    res = computeMultiplicativeInverse(a,a,mod)
+    res = binPow(a,254,mod)
     return hex(res).split('x')[1]
 
 def getSboxValue(b,c):
@@ -79,9 +84,41 @@ def getSboxValue(b,c):
 		res += str(int(b[i]) ^ int(b[(i+4)%8]) ^ int(b[(i+5)%8]) ^ int(b[(i+6)%8]) ^ int(b[(i+7)%8]) ^ int(c[i]))
 	return res
 
+def individualValue(a):
+	b = getMultiplicativeInverse(a)
+	b = int("0x"+b,0) #Ya es entero
+	b = getString(b)# Sacamos la cadena binaria
+	b = b[::-1]# Volteamos esa cadena
+	# Llenamos de ceros si hacen falta
+	while(len(b) < 8):
+		b+='0'
+	c = "11000110"# Definida por default
+	ans = getSboxValue(b,c)
+	ans = ans[::-1]# Volteamos lo obtenido
+	ans = getNumber(ans)# Obtenemos el entero de la cadena binaria
+	#return str(ans)
+	ans = hex(ans)# Obtenemos el hexa del entero
+	return str(ans).split('x')[1]
+	#print("Answer: " + str(ans).split('x')[1])
+
+def generateSbox():
+	alpha = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+	for i in range(len(alpha)):
+		for j in range(len(alpha)):
+			if(i == 0 and j == 0):
+				print(str(hex(99)).split('x')[1],end = " ")
+				#print("99",end = " ")
+			else:
+				print(individualValue(alpha[i]+alpha[j]), end = " ")
+		print("")
+
+def clearTerminal():
+    subprocess.call("cls", shell=True)
 
 def main():
+	clearTerminal()
 	opc = int(input("1.- Multiplication in a binary field\n2.- Get the polynomial representation \n3.- Get the S-box value \nInput:"))
+	clearTerminal()
 	if(opc == 1):
 		print("All the values should be introduced using a binary representation")
 		mod = input("Introduce the irreducible polynomial:")
@@ -99,21 +136,12 @@ def main():
 		ans = getPoly(a)
 		print("Answer: "+str(ans))
 	else:
-		print("All the values should be introduced using HEXADECIMAL values")
-		a = input("Introduce number A: ")
-		b = getMultiplicativeInverse(a)
-		print("Its multiplicative inverse is "+str(b))
-		b = int("0x"+b,0) #Ya es entero
-		b = getString(b)# Sacamos la cadena binaria
-		b = b[::-1]# Volteamos esa cadena
-		# Llenamos de ceros si hacen falta
-		while(len(b) < 8):
-			b+='0'
-		c = "11000110"# Definida por default
-		ans = getSboxValue(b,c)
-		ans = ans[::-1]# Volteamos lo obtenido
-		ans = getNumber(ans)# Obtenemos el entero de la cadena binaria
-		ans = hex(ans)# Obtenemos el hexa del entero
-		print("Answer: " + str(ans))
+		generateSbox()
     
+def prueba():
+	a = input()
+	print(getMultiplicativeInverse(a))
+
+
+#prueba()
 main()
